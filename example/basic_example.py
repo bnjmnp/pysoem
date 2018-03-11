4f32bd0552323701a4446bd2615c316dd6a92ff9
Benjamin Partzsch <benjamin_partzsch@web.de>
2018-03-11 12:27:57 +0100

    more clean basic example

--- example/basic_example.py
+++ example/basic_example.py
@@ -27,6 +27,7 @@ class BasicExample:
         self._ifname = ifname
         self._pd_thread_stop_event = threading.Event()
         self._ch_thread_stop_event = threading.Event()
+        self._actual_wkc = 0
         self._master = pysoem.Master()
         self._master.in_op = False
         self._master.do_check_state = False
@@ -70,75 +71,116 @@ class BasicExample:
                 print('incorrect wkc')
             time.sleep(0.01)
 
+    def _pdo_update_loop(self):
+
+        self._master.in_op = True
+
+        output_len = len(self._master.slaves[2].output)
+
+        tmp = bytearray([0 for i in range(output_len)])
+
+        toggle = True
+        try:
+            while 1:
+                if toggle:
+                    tmp[0] = 0x00
+                else:
+                    tmp[0] = 0x02
+                self._master.slaves[2].output = bytes(tmp)
+
+                toggle ^= True
+
+                time.sleep(1)
+
+        except KeyboardInterrupt:
+            # ctrl-C abort handling
+            print('stopped')
+
+
     def run(self):
 
         self._master.open(self._ifname)
 
-        if self._master.config_init() > 0:
-
-            for i, slave in enumerate(self._master.slaves):
-                assert(slave.man == self.BECKHOFF_VENDOR_ID)
-                assert(slave.id == self._expected_slave_layout[i].product_code)
-                slave.config_func = self._expected_slave_layout[i].config_func
-                slave.is_lost = False
-
-            self._master.config_map()
-
-            if self._master.state_check(pysoem.SAFEOP_STATE, 50000) != pysoem.SAFEOP_STATE:
-                raise Exception('not all slaves reached SAFEOP state')
-
-            self._master.state = pysoem.OP_STATE
-
-            check_thread = threading.Thread(target=self._check_thread)
-            check_thread.start()
-            proc_thread = threading.Thread(target=self._processdata_thread)
-            proc_thread.start()
-
-            self._master.write_state()
-
-            for i in range(40):
-                self._master.state_check(pysoem.OP_STATE, 50000)
-                if self._master.state == pysoem.OP_STATE:
-                    break
-
-            if self._master.state == pysoem.OP_STATE:
-
-                self._master.in_op = True
-
-                output_len = len(self._master.slaves[2].output)
-
-                tmp = bytearray([0 for i in range(output_len)])
-
-                toggle = True
-                try:
-                    while 1:
-                        if toggle:
-                            tmp[0] = 0x00
-                        else:
-                            tmp[0] = 0x02
-                        self._master.slaves[2].output = bytes(tmp)
-
-                        toggle ^= True
-
-                        time.sleep(1)
-
-                except KeyboardInterrupt:
-                    # ctrl-C abort handling
-                    print('stopped')
-                except Exception as expt:
-                    print(expt)
-            else:
-                print('not all slaves reached OP state')
-
-            self._pd_thread_stop_event.set()
-            self._ch_thread_stop_event.set()
-            proc_thread.join()
-            check_thread.join()
-            self._master.state = pysoem.INIT_STATE
-            # request INIT state for all slaves
-            self._master.write_state()
+        if not self._master.config_init() > 0:
             self._master.close()
+            raise BasicExampleError('no slave found')
 
+        for i, slave in enumerate(self._master.slaves):
+            if not ((slave.man == self.BECKHOFF_VENDOR_ID) and
+                    (slave.id == self._expected_slave_layout[i].product_code)):
+                self._master.close()
+                raise BasicExampleError('unexpected slave layout')
+            slave.config_func = self._expected_slave_layout[i].config_func
+            slave.is_lost = False
+
+        self._master.config_map()
+
+        if self._master.state_check(pysoem.SAFEOP_STATE, 50000) != pysoem.SAFEOP_STATE:
+            self._master.close()
+            raise BasicExampleError('not all slaves reached SAFEOP state')
+
+        self._master.state = pysoem.OP_STATE
+
+        check_thread = threading.Thread(target=self._check_thread)
+        check_thread.start()
+        proc_thread = threading.Thread(target=self._processdata_thread)
+        proc_thread.start()
+
+        self._master.write_state()
+
+        all_slaves_reached_op_state = False
+        for i in range(40):
+            self._master.state_check(pysoem.OP_STATE, 50000)
+            if self._master.state == pysoem.OP_STATE:
+                all_slaves_reached_op_state = True
+                break
+
+        if all_slaves_reached_op_state:
+            self._pdo_update_loop()
+
+        self._pd_thread_stop_event.set()
+        self._ch_thread_stop_event.set()
+        proc_thread.join()
+        check_thread.join()
+        self._master.state = pysoem.INIT_STATE
+        # request INIT state for all slaves
+        self._master.write_state()
+        self._master.close()
+
+        if not all_slaves_reached_op_state:
+            raise BasicExampleError('not all slaves reached OP state')
+
+    @staticmethod
+    def _check_slave(slave, pos):
+        if slave.state == (pysoem.SAFEOP_STATE + pysoem.STATE_ERROR):
+            print(
+                'ERROR : slave {} is in SAFE_OP + ERROR, attempting ack.'.format(pos))
+            slave.state = pysoem.SAFEOP_STATE + pysoem.STATE_ACK
+            slave.write_state()
+        elif slave.state == pysoem.SAFEOP_STATE:
+            print(
+                'WARNING : slave {} is in SAFE_OP, try change to OPERATIONAL.'.format(pos))
+            slave.state = pysoem.OP_STATE
+            slave.write_state()
+        elif slave.state > pysoem.NONE_STATE:
+            if slave.reconfig():
+                slave.is_lost = False
+                print('MESSAGE : slave {} reconfigured'.format(pos))
+        elif not slave.is_lost:
+            slave.state_check(pysoem.OP_STATE)
+            if slave.state == pysoem.NONE_STATE:
+                slave.is_lost = True
+                print('ERROR : slave {} lost'.format(pos))
+        if slave.is_lost:
+            if slave.state == pysoem.NONE_STATE:
+                if slave.recover():
+                    slave.is_lost = False
+                    print(
+                        'MESSAGE : slave {} recovered'.format(pos))
+            else:
+                slave.is_lost = False
+                print('MESSAGE : slave {} found'.format(pos))
+    
     def _check_thread(self):
 
         while not self._ch_thread_stop_event.is_set():
@@ -148,47 +190,28 @@ class BasicExample:
                 for i, slave in enumerate(self._master.slaves):
                     if slave.state != pysoem.OP_STATE:
                         self._master.do_check_state = True
-                        if slave.state == (pysoem.SAFEOP_STATE + pysoem.STATE_ERROR):
-                            print(
-                                'ERROR : slave {} is in SAFE_OP + ERROR, attempting ack.'.format(i))
-                            slave.state = pysoem.SAFEOP_STATE + pysoem.STATE_ACK
-                            slave.write_state()
-                        elif slave.state == pysoem.SAFEOP_STATE:
-                            print(
-                                'WARNING : slave {} is in SAFE_OP, try change to OPERATIONAL.'.format(i))
-                            slave.state = pysoem.OP_STATE
-                            slave.write_state()
-                        elif slave.state > pysoem.NONE_STATE:
-                            if slave.reconfig():
-                                slave.is_lost = False
-                                print('MESSAGE : slave {} reconfigured'.format(i))
-                        elif not slave.is_lost:
-                            slave.state_check(pysoem.OP_STATE)
-                            if slave.state == pysoem.NONE_STATE:
-                                slave.is_lost = True
-                                print('ERROR : slave {} lost'.format(i))
-                        if slave.is_lost:
-                            if slave.state == pysoem.NONE_STATE:
-                                if slave.recover():
-                                    slave.is_lost = False
-                                    print(
-                                        'MESSAGE : slave {} recovered'.format(i))
-                            else:
-                                slave.is_lost = False
-                                print('MESSAGE : slave {} found'.format(i))
+                        BasicExample._check_slave(slave, i)
                 if not self._master.do_check_state:
                     print('OK : all slaves resumed OPERATIONAL.')
             time.sleep(0.01)
 
 
+class BasicExampleError(Exception):
+    def __init__(self, message):
+        super(BasicExampleError, self).__init__(message)
+        self.message = message
+
+
 if __name__ == '__main__':
 
-    print('script started')
+    print('basic_example started')
 
     if len(sys.argv) > 1:
         try:
             BasicExample(sys.argv[1]).run()
-        except Exception as expt:
-            print(expt)
+        except BasicExampleError as expt:
+            print('basic_example failed: ' + expt.message)
+            sys.exit(1)
     else:
         print('give ifname as script argument')
+        sys.exit(1)
