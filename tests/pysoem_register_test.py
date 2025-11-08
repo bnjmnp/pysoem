8b98cca844d72b7652f23798cecc2cb6d77b93d8
bnjmnp <benjamin_partzsch@web.de>
2025-11-08 21:17:20 +0100

    Tests: Add test code that checks the `get_watchdog()` function

--- tests/pysoem_register_test.py
+++ tests/pysoem_register_test.py
@@ -10,6 +10,10 @@ def watchdog_device(pysoem_env):
 
 @pytest.fixture
 def watchdog_register_fix(watchdog_device):
+    """Fixture to clean up after the test_watchdog_update() did run.
+    
+    Saves the watchdog settings prior to the test run and restores them after the test run.
+    """
     timeout_ms = 4000
     assert watchdog_device._fprd(0x400, 2, timeout_ms) == bytes([0xC2, 0x09])
     old_wd_time_pdi = watchdog_device._fprd(0x410, 2, timeout_ms)
@@ -37,8 +41,11 @@ def test_watchdog_update(watchdog_device, watchdog_register_fix, wd, time_ms, ex
         'processdata': 0x420,
     }
     if isinstance(expected_reg_value, AttributeError):
-        with pytest.raises(AttributeError):
+        with pytest.raises(AttributeError) as exec_info:
             watchdog_device.set_watchdog(wd_type=wd, wd_time_ms=time_ms)
+        assert exec_info.value.args[0] == "wd_time_ms is limited to 6553.5 ms"
     else:
         watchdog_device.set_watchdog(wd_type=wd, wd_time_ms=time_ms)
         assert watchdog_device._fprd(address=wd_reg[wd], size=2, timeout_us=4000) == expected_reg_value.to_bytes(2, byteorder='little', signed=False)
+        watchdog_divider = 100000
+        assert watchdog_device.get_watchdog(wd_type=wd) == expected_reg_value*watchdog_divider/1000000.0
